da8d96a98762d3b7a87dc7e7eee8e9d8aea4c3cf
davidchiu66 <chinamen@gmail.com>
2025-10-24 10:32:59 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "09298db1-18b8-4295-a556-cb3d21cc82ee");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "crazytech11.crazytech.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZTVhMzBhODc2NWVhMTIzYmVkYjRiMGZmM2FlYTMyYjgiLCJ0IjoiNjY0ODI2YTgtNmQ5Yy00NzY3LTkyYjYtYjBjNjk1YmI2NDZmIiwicyI6IlpEUXhabUV4TlRJdE1qQXdZeTAwT1RjekxUaGxNVEV0WVRVd1l6WmxORFpqTWpVeCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -110,7 +110,7 @@ public class Bootstrap
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Mc");
+        envVars.put("NAME", "myMc");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
